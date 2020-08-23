ef0f4c524aa8d066dfc005bcdc13b96ab5df3b22
Amlan Saha Kundu <skamlan99@gmail.com>
2020-08-23 14:50:50 +0530

    Update data.dart

--- lib/data.dart
+++ lib/data.dart
@@ -6,8 +6,8 @@ List<String> images = [
 ];
 
 List<String> title = [
-  "Hounted Ground",
-  "Fallen In Love",
-  "The Dreaming Moon",
-  "Jack the Persian and the Black Castel",
+  "Feluda",
+  "Byomkesh",
+  "Sherlock",
+  "Professor Shanku",
 ];
